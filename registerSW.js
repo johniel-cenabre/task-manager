7d34750fdda3533961a62280e7d2ee1973d10fec
johniel-cenabre <johniel.judah.cenabre@gmail.com>
2026-03-03 20:01:34 +0800

    remove service worker base

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/task-manager/sw.js', { scope: '/task-manager/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: 'autoUpdate',
-      base: '/task-manager/',
+      base: '/',
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg}']
       },

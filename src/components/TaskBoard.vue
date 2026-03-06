f6de523fc8e9424c9a4cbb6141157ed3241a97ca
johniel-cenabre <johniel.judah.cenabre@gmail.com>
2026-03-06 11:59:20 +0800

    fix paddings

--- src/components/TaskBoard.vue
+++ src/components/TaskBoard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+  <div class="max-w-7xl mx-auto px-2 sm:px-4 lg:px-6 py-4">
     <div class="mb-6 flex items-center justify-between">
       <h2 class="text-lg font-bold text-gray-900 dark:text-white">Task Board</h2>
       <GradientButton
